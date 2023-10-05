549a512f88a18112a2a477d194ea293b6db08ee1
jose antonio <jsancesc@gmail.com>
2023-10-05 13:21:10 +0200

    2 hecho

--- Ej2.py
+++ Ej2.py
@@ -1,1 +1,22 @@
 peso = int(input("Escriba el peso maximo por gondola: "))
+n = int(input("Escriba el numero de niños: "))
+
+niños = list(map(int, input("Escribe el peso de los niños: ").split()))
+
+niños.sort()
+contador = 0
+while True:
+    
+    try:
+        if peso > niños[contador]+niños[contador+1]:
+            niños[contador] += niños[contador+1]
+            niños.pop(contador+1)
+        elif peso <= niños[contador]:
+            contador += 1
+        else:
+            print(niños)
+            break
+    except IndexError:
+        break
+print(len(niños))
+    
